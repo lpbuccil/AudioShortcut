4b4d08e71c6ddd823a72f14628fe6e31aa0b86b2
Unknown <lpbuccil@mtu.edu>
2018-04-25 18:46:37 -0400

    Creates a shortcut with the ability to switch sound devices

---
+++ Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.audioDeviceCB = New System.Windows.Forms.ListBox()
+        Me.createShortcutBtn = New System.Windows.Forms.Button()
+        Me.installBtn = New System.Windows.Forms.Button()
+        Me.setLoactionbtn = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'audioDeviceCB
+        '
+        Me.audioDeviceCB.FormattingEnabled = True
+        Me.audioDeviceCB.Location = New System.Drawing.Point(12, 16)
+        Me.audioDeviceCB.Name = "audioDeviceCB"
+        Me.audioDeviceCB.Size = New System.Drawing.Size(141, 160)
+        Me.audioDeviceCB.TabIndex = 0
+        '
+        'createShortcutBtn
+        '
+        Me.createShortcutBtn.Enabled = False
+        Me.createShortcutBtn.Location = New System.Drawing.Point(254, 153)
+        Me.createShortcutBtn.Name = "createShortcutBtn"
+        Me.createShortcutBtn.Size = New System.Drawing.Size(101, 23)
+        Me.createShortcutBtn.TabIndex = 1
+        Me.createShortcutBtn.Text = "Create Shortcut"
+        Me.createShortcutBtn.UseVisualStyleBackColor = True
+        '
+        'installBtn
+        '
+        Me.installBtn.Location = New System.Drawing.Point(254, 31)
+        Me.installBtn.Name = "installBtn"
+        Me.installBtn.Size = New System.Drawing.Size(101, 21)
+        Me.installBtn.TabIndex = 2
+        Me.installBtn.Text = "Install NirCMD"
+        Me.installBtn.UseVisualStyleBackColor = True
+        '
+        'setLoactionbtn
+        '
+        Me.setLoactionbtn.Location = New System.Drawing.Point(254, 58)
+        Me.setLoactionbtn.Name = "setLoactionbtn"
+        Me.setLoactionbtn.Size = New System.Drawing.Size(101, 47)
+        Me.setLoactionbtn.TabIndex = 3
+        Me.setLoactionbtn.Text = "Locate nircmd.exe Parent Directory"
+        Me.setLoactionbtn.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 181)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(367, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'status
+        '
+        Me.status.Name = "status"
+        Me.status.Size = New System.Drawing.Size(0, 17)
+        '
+        'btnHelp
+        '
+        Me.btnHelp.Location = New System.Drawing.Point(159, 155)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(38, 21)
+        Me.btnHelp.TabIndex = 5
+        Me.btnHelp.Text = "Info"
+        Me.btnHelp.UseVisualStyleBackColor = True
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 203)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.setLoactionbtn)
+        Me.Controls.Add(Me.installBtn)
+        Me.Controls.Add(Me.createShortcutBtn)
+        Me.Controls.Add(Me.audioDeviceCB)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "main"
+        Me.Text = "Audio Shortcut"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents audioDeviceCB As ListBox
+    Friend WithEvents createShortcutBtn As Button
+    Friend WithEvents installBtn As Button
+    Friend WithEvents setLoactionbtn As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents status As ToolStripStatusLabel
+    Friend WithEvents btnHelp As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,246 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports Microsoft.Win32
+
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Net
+Imports System.Windows.Forms.VisualStyles
+Imports IWshRuntimeLibrary
+
+
+
+Public Class main
+
+    Dim selected As Boolean = False
+    Dim nircmd As Boolean = False
+    Dim NirCMDPath As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+        Try
+            Using myKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("MMDevices").OpenSubKey("Audio").OpenSubKey("Render")
+                Try
+                    For Each reg As String In myKey.GetSubKeyNames()
+                        Try
+                            If (myKey.OpenSubKey(reg).GetValue("DeviceState") = 1) Then
+                                Try
+                                    myKey.OpenSubKey(reg).OpenSubKey("Properties").GetValue("{a45c254e-df1c-4efd-8020-67d146a850e0}")
+
+                                    audioDeviceCB.Items.Add(myKey.OpenSubKey(reg).OpenSubKey("Properties").GetValue("{a45c254e-df1c-4efd-8020-67d146a850e0},2"))
+                                Catch ex As Exception
+                                End Try
+                            End If
+                        Catch ex As Exception
+                        End Try
+                    Next
+                Catch ex As Exception
+                End Try
+            End Using
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub createShortcutBtn_Click(sender As Object, e As EventArgs) Handles createShortcutBtn.Click
+
+        Dim name As String
+
+        name = audioDeviceCB.SelectedItem.ToString().Replace(" ", "").Replace("-", "")
+        Dim file As FileInfo = New FileInfo(NirCMDPath + "\" + name + ".bat")
+
+        If (file.Exists) Then
+            status.Text = "File exists: " + NirCMDPath + "\" + name + ".bat"
+            Threading.Thread.Sleep(500)
+            Me.Update()
+            file.Delete()
+            status.Text = "Removed: " + NirCMDPath + "\" + name + ".bat"
+            Me.Update()
+        End If
+
+
+
+        status.Text = "Creating Batch: " + NirCMDPath + "\" + name + ".bat"
+        Threading.Thread.Sleep(500)
+        Me.Update()
+
+        Dim fs As StreamWriter
+        fs = New StreamWriter(file.ToString(), True)
+        fs.WriteLine("@ECHO OFF")
+        Dim cc = "\NIRCMDC"
+        Dim command = "setdefaultsounddevice"
+        fs.WriteLine($"{NirCMDPath}{cc} {command} ""{name}"" 1 ")
+        fs.WriteLine($"{NirCMDPath}{cc} {command} ""{name}"" 2 ")
+        fs.Close()
+
+        status.Text = "Batch created"
+        Threading.Thread.Sleep(500)
+        Me.Update()
+
+
+        Dim wsh = CreateObject("WScript.Shell")
+        Dim MyShortcut As WshShortcut
+        Dim DesktopPath
+        DesktopPath = wsh.SpecialFolders("Desktop")
+
+        Dim temp As FileInfo = New FileInfo(DesktopPath + $"\{name}.lnk")
+        If (temp).Exists Then
+            temp.Delete()
+        End If
+
+        MyShortcut = wsh.CreateShortcut($"{DesktopPath}\{name}.lnk")
+
+        Dim path = $"{NirCMDPath}\{name}.bat\"
+        MyShortcut.TargetPath = path
+
+        MyShortcut.WorkingDirectory = wsh.ExpandEnvironmentStrings(NirCMDPath)
+        MyShortcut.WindowStyle = 4
+        MyShortcut.Save()
+
+        status.Text = "Shortcut created"
+        Threading.Thread.Sleep(500)
+        Me.Update()
+
+
+    End Sub
+
+
+    Private Sub installBtn_Click(sender As Object, e As EventArgs) Handles installBtn.Click
+        Dim folderBrowser As New FolderBrowserDialog
+        Dim NirCMDWebsite As String = "http://www.nirsoft.net/utils/nircmd.zip"
+        Dim pathString As String = ""
+        Dim folderFile As String = "\NirCMD"
+        Dim fullpath As String = ""
+
+
+        folderBrowser.ShowDialog()
+        If (folderBrowser.SelectedPath = Nothing) Then
+            status.Text = "Select an install location"
+            Me.Update()
+            Exit Sub
+        End If
+        pathString = folderBrowser.SelectedPath
+        status.Text = "Path set to: " + pathString
+        Me.Update()
+        Threading.Thread.Sleep(500)
+
+        Dim directory As DirectoryInfo = New DirectoryInfo(pathString + folderFile)
+        directory.Create()
+
+
+        status.Text = "Created directory: " + pathString + folderFile
+        Me.Update()
+        Threading.Thread.Sleep(500)
+
+        status.Text = "Downloading Nircmd to : " + pathString + folderFile
+        Me.Update()
+        Threading.Thread.Sleep(500)
+
+        Dim webClient As New WebClient
+        webClient.DownloadFile(NirCMDWebsite, pathString + folderFile + "\nircmd.zip")
+
+        status.Text = "Downloaded"
+        Me.Update()
+        Threading.Thread.Sleep(500)
+
+        fullpath = pathString + folderFile
+
+        status.Text = "Unpacking"
+        Me.Update()
+        Threading.Thread.Sleep(500)
+
+        If (New FileInfo(fullpath + "\nircmdc.exe").Exists) Then
+            status.Text = "File nircmdc.exe exists, removing"
+            Me.Update()
+            Threading.Thread.Sleep(250)
+            Dim file1 As FileInfo = New FileInfo(fullpath + "\nircmdc.exe")
+            file1.Delete()
+        End If
+
+        If (New FileInfo(fullpath + "\NirCmd.chm").Exists) Then
+            status.Text = "File NirCmd.chm exists, removing"
+            Me.Update()
+            Threading.Thread.Sleep(250)
+            Dim file1 As FileInfo = New FileInfo(fullpath + "\NirCmd.chm")
+            file1.Delete()
+        End If
+
+        If (New FileInfo(fullpath + "\nircmd.exe").Exists) Then
+            status.Text = "File nircmd.exe exists, removing"
+            Me.Update()
+            Threading.Thread.Sleep(250)
+            Dim file1 As FileInfo = New FileInfo(fullpath + "\nircmd.exe")
+            file1.Delete()
+        End If
+
+        ZipFile.ExtractToDirectory(fullpath + "\nircmd.zip", fullpath)
+
+        Dim file As FileInfo = New FileInfo(fullpath + "\nircmd.zip")
+        file.Delete()
+
+
+        NirCMDPath = fullpath
+
+        status.Text = "NirCMD path set to : " + fullpath
+        Me.Update()
+
+        nircmd = True
+        If (nircmd = True And selected = True) Then
+            createShortcutBtn.Enabled = True
+        End If
+    End Sub
+
+    Private Sub setLoactionbtn_Click(sender As Object, e As EventArgs) Handles setLoactionbtn.Click
+
+        Dim folderBrowser As New FolderBrowserDialog
+        folderBrowser.ShowDialog()
+        Dim pathString = folderBrowser.SelectedPath
+        If (Not New FileInfo(pathString + "\NirCmd.chm").Exists) Then
+            status.Text = "Missing " + pathString + "\NirCmd.chm"
+            Me.Update()
+            Exit Sub
+        End If
+
+        If (Not New FileInfo(pathString + "\nircmd.exe").Exists) Then
+            status.Text = "Missing " + pathString + "\nircmd.exe."
+            Me.Update()
+            Exit Sub
+        End If
+
+        If (Not New FileInfo(pathString + "\nircmdc.exe").Exists) Then
+            status.Text = "Missing " + pathString + "\nircmdc.exe."
+            Me.Update()
+            Exit Sub
+        End If
+
+        status.Text = "Path set to: " + pathString
+        Me.Update()
+        Threading.Thread.Sleep(500)
+
+        NirCMDPath = pathString
+        nircmd = True
+        If (nircmd = True And selected = True) Then
+            createShortcutBtn.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub audioDeviceCB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles audioDeviceCB.SelectedIndexChanged
+
+        If (audioDeviceCB.SelectedItems.Count = 1) Then
+            selected = True
+
+            If (nircmd = True And selected = True) Then
+                createShortcutBtn.Enabled = True
+            End If
+        Else
+            selected = False
+        End If
+
+
+    End Sub
+
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        MessageBox.Show("To make shortcut pinnable to taskbar, add ""%SystemRoot%\system32\cmd.exe /c"" to the font of the target path.")
+    End Sub
+End Class
